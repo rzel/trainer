349b5e8455b31b883ee3fabd86cc59c1a067ccdf
nutszebra <nutszebra@gmail.com>
2017-06-11 02:52:27 +0900

    start using _device_id

--- nutszebra_ilsvrc_object_localization_with_multi_gpus.py
+++ nutszebra_ilsvrc_object_localization_with_multi_gpus.py
@@ -19,16 +19,14 @@ da = nutszebra_data_augmentation_picture.DataAugmentationPicture()
 utility = nutszebra_utility.Utility()
 
 
-async def calculate_loss_and_accuracy(models, X, T, gpus, train, divider=1.0):
-    def execute(model, x, t, train, gpu, divider):
-        x = model.prepare_input(x, dtype=np.float32, volatile=not train, gpu=gpu)
-        t = model.prepare_input(t, dtype=np.int32, volatile=not train, gpu=gpu)
-        x.to_gpu(gpu)
-        t.to_gpu(gpu)
+async def calculate_loss_and_accuracy(models, X, T, train, divider=1.0):
+    def execute(model, x, t, train, divider):
+        x = model.prepare_input(x, dtype=np.float32, volatile=not train, gpu=model._device_id)
+        t = model.prepare_input(t, dtype=np.int32, volatile=not train, gpu=model._device_id)
         y = model(x, train=train)
         return model.calc_loss(y, t) / divider, model.accuracy_n(y, t, n=5)
     n_img = int(float(len(X)) / len(models))
-    cors = [execute(models[i], X[i * n_img: (i + 1) * n_img], T[i * n_img: (i + 1) * n_img], gpus[i], train, divider) for i in six.moves.range(len(models))]
+    cors = [execute(models[i], X[i * n_img: (i + 1) * n_img], T[i * n_img: (i + 1) * n_img], train, divider) for i in six.moves.range(len(models))]
     results = await asyncio.gather(*cors)
     return results
 
@@ -142,7 +140,6 @@ class TrainIlsvrcObjectLocalizationClassificationWithMultiGpus(object):
     def train_one_epoch(self):
         # initialization
         log = self.log
-        gpus = self.gpus
         models = self.models
         optimizer = self.optimizer
         train_x = self.train_x
@@ -173,7 +170,7 @@ class TrainIlsvrcObjectLocalizationClassificationWithMultiGpus(object):
                 tmp_x = Da.zero_padding(tmp_x)
                 n_img = int(float(len(tmp_x)) / n_parallel)
                 # calculate loss and accuracy
-                results = loop.run_until_complete(calculate_loss_and_accuracy(models, tmp_x, tmp_t, gpus, True, n_img * train_batch_divide))
+                results = loop.run_until_complete(calculate_loss_and_accuracy(models, tmp_x, tmp_t, True, n_img * train_batch_divide))
                 losses, _ = list(six.moves.zip(*results))
                 # backward
                 loop.run_until_complete(backward(losses))
