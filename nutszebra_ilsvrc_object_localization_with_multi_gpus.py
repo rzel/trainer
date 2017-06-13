944144e868d3b205f6b78623700b7e679e348285
nutszebra <nutszebra@gmail.com>
2017-06-13 19:57:36 +0900

    nccl

--- nutszebra_ilsvrc_object_localization_with_multi_gpus.py
+++ nutszebra_ilsvrc_object_localization_with_multi_gpus.py
@@ -2,6 +2,7 @@ import six
 import itertools
 import numpy as np
 from chainer import serializers
+from chainer import cuda
 import nutszebra_log2
 import nutszebra_utility
 import nutszebra_sampling
@@ -11,64 +12,231 @@ import nutszebra_data_augmentation_picture
 import nutszebra_data_augmentation
 import nutszebra_basic_print
 import multiprocessing
-multiprocessing.set_start_method('spawn')
+
+try:
+    from cupy.cuda import nccl
+    _available = True
+except ImportError:
+    _available = False
 
 Da = nutszebra_data_augmentation_picture.DataAugmentationPicture()
 sampling = nutszebra_sampling.Sampling()
 preprocess = nutszebra_preprocess_picture.PreprocessPicture()
 da = nutszebra_data_augmentation_picture.DataAugmentationPicture()
 utility = nutszebra_utility.Utility()
-
-
 X = []
 T = []
-M = []
 Loss = []
 Divider = []
 Train = []
 
-
-def _addgrads(i):
-    M[0].addgrads(M[i])
-    return True
-
-
-def _copyparams(i):
-    M[i].copyparams(M[0])
-    return True
+"""
+https://github.com/chainer/chainer/blob/master/chainer/training/updaters/multiprocess_parallel_updater.py
+"""
 
 
-def _forward_and_backward(i):
-    model, x, t = M[i], X[i], T[i]
-    x = model.prepare_input(x, dtype=np.float32, volatile=False, gpu=model._device_id)
-    t = model.prepare_input(t, dtype=np.int32, volatile=False, gpu=model._device_id)
-    y = model(x, train=Train[0])
-    loss = Loss[i](y, t) / Divider[0]
-    loss.backward()
-    loss.to_cpu()
-    return float(loss.data)
+class _Worker(multiprocessing.Process):
+
+    def __init__(self, process_id, pipe, model, gpus):
+        super(_Worker, self).__init__()
+        self.process_id = process_id
+        self.pipe = pipe
+        self.model = model
+        self.device = gpus[process_id]
+        self.number_of_devices = len(gpus)
+
+    def setup(self):
+        _, communication_id = self.pipe.recv()
+        self.communication = nccl.NcclCommunicator(self.number_of_devices,
+                                                   communication_id,
+                                                   self.process_id)
+        self.model.to_gpu(self.device)
+
+    def run(self):
+        dev = cuda.Device(self.device)
+        dev.use()
+        # build communication via nccl
+        self.setup()
+        gp = None
+        while True:
+            job, data = self.pipe.recv()
+            if job == 'finalize':
+                dev.synchronize()
+                break
+            if job == 'update':
+                # for reducing memory
+                self.model.cleargrads()
+                train = Train[0]
+                x = self.model.prepare_input(X[self.device], dtype=np.float32, volatile=not train, gpu=self.device)
+                t = self.model.prepare_input(T[self.device], dtype=np.int32, volatile=not train, gpu=self.device)
+                y = self.model(x, train=train)
+                loss = self.model.calc_loss(y, t) / Divider[0]
+                loss.backward()
+                loss.to_cpu()
+                Loss.append(float(loss.data))
+
+                del x
+                del t
+                del y
+                del loss
+
+                # send gradients of self.model
+                gg = gather_grads(self.model)
+                null_stream = cuda.Stream.null
+                self.communication.reduce(gg.data.ptr,
+                                          gg.data.ptr,
+                                          gg.size,
+                                          nccl.NCCL_FLOAT,
+                                          nccl.NCCL_SUM,
+                                          0,
+                                          null_stream.ptr)
+                del gg
+                # send parameters of self.model
+                gp = gather_params(self.model)
+                self.communication.bcast(gp.data.ptr,
+                                         gp.size,
+                                         nccl.NCCL_FLOAT,
+                                         0,
+                                         null_stream.ptr)
+                scatter_params(self.model, gp)
+                gp = None
 
 
-class Execute(object):
+def size_num_grads(link):
+    """Count total size of all gradient arrays of a given link
+    Args:
+        link (chainer.link.Link): Target link object.
+    """
+    size = 0
+    num = 0
+    for param in link.params():
+        if param.size == 0:
+            continue
+        size += param.size
+        num += 1
+    return size, num
 
-    def __init__(self):
-        pass
 
-    def execute(self, func, indices):
-        p = multiprocessing.Pool(len(indices))
-        results = p.starmap(func, indices)
-        # results = []
-        # for i in indices:
-        #     results.append(func(i[0]))
-        return results
+def _batch_memcpy():
+    return cuda.cupy.ElementwiseKernel(
+        'raw T ptrs, raw X info',
+        'raw float32 dst',
+        '''
+            int id_min = id_pre;
+            int id_max = num_src;
+            while (id_max - id_min > 1) {
+                int id = (id_max + id_min) / 2;
+                if (i < info[id]) id_max = id;
+                else              id_min = id;
+            }
+            int id = id_min;
+            float *src = (float *)(ptrs[id]);
+            int i_dst = i;
+            int i_src = i;
+            if (id > 0) i_src -= info[id];
+            dst[i_dst] = 0;
+            if (src != NULL) {
+                dst[i_dst] = src[i_src];
+            }
+            id_pre = id;
+        ''',
+        'batch_memcpy',
+        loop_prep='''
+                int num_src = info[0];
+                int id_pre = 0;
+            ''')
+
+
+def gather_grads(link):
+    """Put together all gradient arrays and make a single array
+    Args:
+        link (chainer.link.Link): Target link object.
+    Return:
+        cupy.ndarray
+    """
+    size, num = size_num_grads(link)
+
+    ptrs = np.empty(num, dtype=np.uint64)
+    info = np.empty(num + 1, dtype=np.int32)
+    info[0] = 0
+    i = 0
+    for param in link.params():
+        if param.size == 0:
+            continue
+        ptrs[i] = 0  # NULL pointer
+        if param.grad is not None:
+            ptrs[i] = param.grad.data.ptr
+        info[i + 1] = info[i] + param.size
+        i += 1
+    info[0] = num
+
+    ptrs = cuda.to_gpu(ptrs, stream=cuda.Stream.null)
+    info = cuda.to_gpu(info, stream=cuda.Stream.null)
+
+    return _batch_memcpy()(ptrs, info, size=size)
+
+
+def gather_params(link):
+    """Put together all gradient arrays and make a single array
+    Args:
+        link (chainer.link.Link): Target link object.
+    Return:
+        cupy.ndarray
+    """
+    size, num = size_num_grads(link)
+
+    ptrs = np.ep(num, dtype=np.uint64)
+    info = np.ep(num + 1, dtype=np.int32)
+    info[0] = 0
+    i = 0
+    for param in link.params():
+        if param.size == 0:
+            continue
+        ptrs[i] = 0  # NULL pointer
+        if param.data is not None:
+            ptrs[i] = param.data.data.ptr
+        info[i + 1] = info[i] + param.size
+        i += 1
+    info[0] = num
+
+    ptrs = cuda.to_gpu(ptrs, stream=cuda.Stream.null)
+    info = cuda.to_gpu(info, stream=cuda.Stream.null)
+
+    return _batch_memcpy()(ptrs, info, size=size)
+
+
+def scatter_grads(link, array):
+    """Put back contents of the specified array to the related gradient arrays
+    Args:
+        link (chainer.link.Link): Target link object.
+        array (cupy.ndarray): gathered array created by gather_grads()
+    """
+    offset = 0
+    for param in link.params():
+        next_offset = offset + param.size
+        param.grad = array[offset:next_offset].reshape(param.data.shape)
+        offset = next_offset
+
+
+def scatter_params(link, array):
+    """Put back contents of the specified array to the related gradient arrays
+    Args:
+        link (chainer.link.Link): Target link object.
+        array (cupy.ndarray): gathered array created by gather_params()
+    """
+    offset = 0
+    for param in link.params():
+        next_offset = offset + param.size
+        param.data = array[offset:next_offset].reshape(param.data.shape)
+        offset = next_offset
 
 
 class TrainIlsvrcObjectLocalizationClassificationWithMultiGpus(object):
 
-    def __init__(self, models=None, optimizer=None, load_models=None, load_optimizer=None, load_log=None, load_data=None, da=nutszebra_data_augmentation.DataAugmentationNormalizeBigger, save_path='./', epoch=100, batch=128, gpus=(0, 1, 2, 3), start_epoch=1, train_batch_divide=2, test_batch_divide=2, small_sample_training=None):
-        self.models = models
+    def __init__(self, model=None, optimizer=None, load_model=None, load_optimizer=None, load_log=None, load_data=None, da=nutszebra_data_augmentation.DataAugmentationNormalizeBigger, save_path='./', epoch=100, batch=128, gpus=(0, 1, 2, 3), start_epoch=1, train_batch_divide=2, test_batch_divide=2, small_sample_training=None):
+        self.models = model
         self.optimizer = optimizer
-        self.load_model = load_models
+        self.load_model = load_model
         self.load_optimizers = load_optimizer
         self.load_log = load_log
         self.load_data = load_data
@@ -86,10 +254,14 @@ class TrainIlsvrcObjectLocalizationClassificationWithMultiGpus(object):
         # Log module
         self.log = self.log_init()
         # initializing
-        [self.model_init(model, load_model, gpu)for model, load_model, gpu in six.moves.zip(models, load_models, gpus)]
+        self.model_init(model, load_model)
         # create directory
         self.save_path = save_path if save_path[-1] == '/' else save_path + '/'
         utility.make_dir('{}model'.format(self.save_path))
+        self._initialized = False
+        self._pipes = []
+        self._workers = []
+        self.communication = None
 
     def data_init(self):
         data = nutszebra_load_ilsvrc_object_localization.LoadDataset(self.load_data)
@@ -133,20 +305,101 @@ class TrainIlsvrcObjectLocalizationClassificationWithMultiGpus(object):
         return log
 
     @staticmethod
-    def model_init(model, load_model, gpu):
+    def model_init(model, load_model):
         if load_model is None:
             print('Weight initialization')
             model.weight_initialization()
         else:
             print('loading {}'.format(load_model))
             serializers.load_npz(load_model, model)
-        model.check_gpu(gpu)
+
+    @staticmethod
+    def available():
+        return _available
+
+    def _send_message(self, message):
+        for pipe in self._pipes:
+            pipe.send(message)
+
+    def setup_workers(self):
+        # work only once
+        if self._initialized:
+            return
+        self._initialized = True
+
+        self.models[0].cleargrads()
+        for i in six.moves.range(1, len(self.gpus)):
+            pipe, worker_end = multiprocessing.Pipe()
+            worker = _Worker(i, worker_end, self)
+            worker.start()
+            self._workers.append(worker)
+            self._pipes.append(pipe)
+
+        with cuda.Device(self.gpus[0]):
+            self.model.to_gpu(self.gpus[0])
+            if len(self.gpus) > 1:
+                communication_id = nccl.get_unique_id()
+                self._send_message(("set comm_id", communication_id))
+                self.communication = nccl.NcclCommunicator(len(self.gpus),
+                                                           communication_id,
+                                                           0)
+
+    def update_core(self):
+        self.setup_workers()
+
+        self._send_message(('update', None))
+        with cuda.Device(self.gpus[0]):
+            # For reducing memory
+            self.model.cleargrads()
+
+            train = Train[0]
+            x = self.model.prepare_input(X[self.gpus[0]], dtype=np.float32, volatile=not train, gpu=self.gpus[0])
+            t = self.model.prepare_input(T[self.gpus[0]], dtype=np.int32, volatile=not train, gpu=self.gpus[0])
+            y = self.model(x, train=train)
+            loss = self.model.calc_loss(y, t) / Divider[0]
+            loss.backward()
+            loss.to_cpu()
+            Loss.append(float(loss.data))
+
+            del x
+            del t
+            del y
+            del loss
+
+            # NCCL: reduce grads
+            null_stream = cuda.Stream.null
+            if self.communication is not None:
+                # send grads
+                gg = gather_grads(self.model)
+                self.communication.reduce(gg.data.ptr,
+                                          gg.data.ptr,
+                                          gg.size,
+                                          nccl.NCCL_FLOAT,
+                                          nccl.NCCL_SUM,
+                                          0,
+                                          null_stream.ptr)
+                # copy grads, gg, to  self.models[0]
+                scatter_grads(self.model, gg)
+                del gg
+            self.optimizer.update()
+            if self.communication is not None:
+                gp = gather_params(self.model)
+                self.communication.bcast(gp.data.ptr,
+                                         gp.size,
+                                         nccl.NCCL_FLOAT,
+                                         0,
+                                         null_stream.ptr)
+
+    def finalize(self):
+        self._send_message(('finalize', None))
+
+        for worker in self._workers:
+            worker.join()
 
     def train_one_epoch(self):
         # initialization
         log = self.log
         models = self.models
-        optimizer = self.optimizer
         train_x = self.train_x
         train_y = self.train_y
         batch = self.batch
@@ -161,9 +414,7 @@ class TrainIlsvrcObjectLocalizationClassificationWithMultiGpus(object):
         Train.append(True)
         Divider.clear()
         Divider.append(n_parallel * train_batch_divide)
-        exe = Execute()
         for _, indices in six.moves.zip(progressbar, yielder):
-            [model.cleargrads() for model in models]
             for ii in six.moves.range(0, len(indices), batch_of_batch):
                 x = train_x[indices[ii:ii + batch_of_batch]]
                 t = train_y[indices[ii:ii + batch_of_batch]]
@@ -175,31 +426,19 @@ class TrainIlsvrcObjectLocalizationClassificationWithMultiGpus(object):
                     if img is not None:
                         tmp_x.append(img)
                         tmp_t.append(t[i])
-
                 tmp_x = Da.zero_padding(tmp_x)
                 # calculate loss and accuracy
                 X.clear()
                 T.clear()
-                M.clear()
                 Loss.clear()
                 n_img = int(float(len(tmp_x)) / len(models))
-                for i in six.moves.range(len(models)):
-                    model = models[i]
+                for gpu in self.gpus:
                     x = tmp_x[i * n_img: (i + 1) * n_img]
                     t = tmp_t[i * n_img: (i + 1) * n_img]
                     X.append(x)
                     T.append(t)
-                    M.append(model)
-                    Loss.append(model.calc_loss)
-                loss = np.sum(exe.execute(_forward_and_backward, [(i,) for i in six.moves.range(len(models))]))
-                # results = calculate_loss(models, tmp_x, tmp_t, True, n_parallel * train_batch_divide)
-                # accumulate grads
-                exe.execute(_addgrads, [(i + 1,) for i in six.moves.range(len(models) - 1)])
-                sum_loss += loss * data_length
-            optimizer.update()
-            print(M[0].linear.linear.linear.W.data)
-            # Synchronized update
-            exe.execute(_copyparams, [(i + 1,) for i in six.moves.range(len(models) - 1)])
+                self.update_core()
+                sum_loss += np.sum(Loss) * Divider[0] / data_length
         log({'loss': float(sum_loss)}, 'train_loss')
         print(log.train_loss())
 
