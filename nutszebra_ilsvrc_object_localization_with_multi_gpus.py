37023653d3391a70f7ead112e4e592212879e021
nutszebra <nutszebra@gmail.com>
2017-06-15 01:05:39 +0900

    pointer

--- nutszebra_ilsvrc_object_localization_with_multi_gpus.py
+++ nutszebra_ilsvrc_object_localization_with_multi_gpus.py
@@ -68,8 +68,8 @@ class _Worker(multiprocessing.Process):
             if job == 'update':
                 # for reducing memory
                 self.model.cleargrads()
-                x = self.X
-                t = self.T
+                x = self.X[0]
+                t = self.T[0]
                 tmp_x = []
                 tmp_t = []
                 for i in six.moves.range(len(x)):
@@ -116,8 +116,8 @@ class _Worker(multiprocessing.Process):
             if job == 'test':
                 # for reducing memory
                 self.model.cleargrads()
-                x = self.X
-                t = self.T
+                x = self.X[0]
+                t = self.T[0]
                 print(len(x))
                 tmp_x = []
                 tmp_t = []
@@ -510,8 +510,8 @@ class TrainIlsvrcObjectLocalizationClassificationWithMultiGpus(object):
                 self.X = list(x[:n_img])
                 self.T = list(t[:n_img])
                 for i in six.moves.range(1, len(gpus)):
-                    self._workers[i - 1].X = list(x[i * n_img: (i + 1) * n_img])
-                    self._workers[i - 1].T = list(t[i * n_img: (i + 1) * n_img])
+                    self._workers[i - 1].X.append(list(x[i * n_img: (i + 1) * n_img]))
+                    self._workers[i - 1].T.append(list(t[i * n_img: (i + 1) * n_img]))
                 self.update_core()
                 sum_loss = np.sum(self.Loss)
         log({'loss': float(sum_loss)}, 'train_loss')
@@ -552,8 +552,8 @@ class TrainIlsvrcObjectLocalizationClassificationWithMultiGpus(object):
             self.X = list(x[:n_img])
             self.T = list(t[:n_img])
             for i in six.moves.range(1, len(gpus)):
-                self._workers[i - 1].X = list(x[i * n_img: (i + 1) * n_img])
-                self._workers[i - 1].T = list(t[i * n_img: (i + 1) * n_img])
+                self._workers[i - 1].X.append(list(x[i * n_img: (i + 1) * n_img]))
+                self._workers[i - 1].T.append(list(t[i * n_img: (i + 1) * n_img]))
             self.test_core()
         for i in six.moves.range(len(self._workers)):
             self.Accuracy += self._workers[i].Accuracy
